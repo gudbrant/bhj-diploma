ada9131704c26a44968b40ed69de231d71b94c70
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:00:50 +0800

    Update createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,53 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (options = {}, callback) => {
+	let xhr = new XMLHttpRequest;
+	let method = options.method;
+	let url;
+	let formData;
+
+	if (method === 'GET') {
+    
+    if(options.data) {
+    	let urlOption = Object.entries(options.data)
+    	.map(([key, value]) => `${key}=${value}`)
+    	.join('&');
+    	url = `${options.url}?${urlOption}`;
+    }
+
+  } else {
+  	formData = new FormData();
+		url = options.url;
+		
+		for (let item in options.data) {
+			formData.append(item, options.data[item]);
+		}
+	}
+
+	xhr.addEventListener('readystatechange', function () {
+		if(this.readyState == xhr.DONE) {
+			if (this.status == 200) {
+				callback(null, JSON.parse(this.responseText));
+			} else {
+				callback(this.responseType, null);
+			}
+		}
+	});
+
+	try {
+		xhr.withCredentials = true;
+		xhr.open(method, url);
+
+		if (formData != undefined) {
+			xhr.send(formData);
+		} else {
+			xhr.send();
+		}		
+	}
+	catch (e) {
+		console.log(e);
+		callback(e);
+	}
 };
