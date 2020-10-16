d99060da9128c52644d65a5907eae610d5a00e8d
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:00:52 +0800

    Update Entity.js

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,16 +1,19 @@
 /**
  * Класс Entity - базовый для взаимодействия с сервером.
  * Имеет свойство URL, равно пустой строке.
+ * Имеет свойство HOST, равно 'https://bhj-diplom.letsdocode.ru'.
  * */
-class Entity {
 
+class Entity {
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
   static list( data, callback = f => f ) {
-
+    let xhr = createRequest(Object.assign({url: this.HOST + this.URL, method: 'GET'}, {data}),(err, data) => {
+      callback(err, data);
+    });
   }
 
   /**
@@ -19,7 +22,10 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create( data, callback = f => f ) {
-
+    data = Object.assign({_method: 'PUT'}, data);
+    const xhr = createRequest(Object.assign({url: this.HOST + this.URL, method: 'POST'}, data), (err, data) => {
+      callback(err, data);
+    });
   }
 
   /**
@@ -27,7 +33,10 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static get( id = '', data, callback = f => f ) {
-
+    data = Object.assign({id}, data);
+    const xhr = createRequest(Object.assign({url: this.HOST + this.URL, method: 'GET'}, {data}), (err, data) => {
+      callback(err, data);
+    });
   }
 
   /**
@@ -35,7 +44,12 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove( id = '', data, callback = f => f ) {
-
+    data = Object.assign({id}, {_method: 'DELETE'}, data);
+    const xhr = createRequest(Object.assign({url: this.HOST + this.URL, method: 'POST'}, {data}), (err, data) => {
+      callback(err, data);
+    });
   }
 }
 
+Entity.HOST = 'https://bhj-diplom.letsdocode.ru';
+Entity.URL = '';
