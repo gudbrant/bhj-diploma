76ac58d205dd9e271b4959004b2592441d806aff
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:01:02 +0800

    Update CreateTransactionForm.js

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -3,13 +3,15 @@
  * создания новой транзакции
  * Наследуется от AsyncForm
  * */
-class CreateTransactionForm {
+class CreateTransactionForm extends AsyncForm {
   /**
    * Вызывает родительский конструктор и
    * метод renderAccountsList
    * */
-  constructor( element ) {
-
+  constructor(element) {
+    super(element);
+    this.element = element;
+    this.renderAccountsList();
   }
 
   /**
@@ -17,7 +19,24 @@ class CreateTransactionForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
+    let elSelect = this.element.querySelector('.accounts-select');
+    let elOPtions;
 
+    if (User.current()) {
+      Account.list(User.current(), (err, data) => {
+
+        if (data.success) {
+
+          for (let i = 0; i < data.data.length; i++) {
+            let datItem = data.data[i];
+            elOPtions += `
+              <option value="${datItem.id}">${datItem.name}</option>
+            `;
+          }
+          elSelect.innerHTML = elOPtions;
+        }
+      });
+    }
   }
 
   /**
@@ -27,6 +46,19 @@ class CreateTransactionForm {
    * в котором находится форма
    * */
   onSubmit( options ) {
+    Transaction.create(options, (err, data) => {
 
+      if (data.success) {
+        this.element.reset();
+
+        let modal = new Modal(this.element.closest('.modal'));
+        modal.close();
+
+        App.update();
+      } else {
+        alert(response.error);
+        return;
+      }
+    });
   }
 }
