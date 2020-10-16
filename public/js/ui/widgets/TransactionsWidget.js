e284737a5e50638c1eb9a89f671ed584eb7007e3
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:01:25 +0800

    Update TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -11,7 +11,13 @@ class TransactionsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if(!element) {
+      throw new Error('Передан пустой элемент');
+    } else {
+      this.element = element;
+      this.registerEvents();
+    }
+    
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -20,6 +26,16 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
-
+    this.element.querySelector('.create-income-button')
+    .addEventListener('click', (event) => {
+      event.preventDefault();
+      App.getModal('newIncome').open();
+    });
+    
+    this.element.querySelector('.create-expense-button')
+    .addEventListener('click', (event) => {
+      event.preventDefault();
+      App.getModal('newExpense').open();
+    });
   }
 }
