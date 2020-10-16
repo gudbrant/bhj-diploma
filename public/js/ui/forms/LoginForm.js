def043ecdb48d93969d845ccc91512e981fe18ff
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:01:04 +0800

    Update LoginForm.js

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -3,7 +3,7 @@
  * входа в портал
  * Наследуется от AsyncForm
  * */
-class LoginForm {
+class LoginForm extends AsyncForm {
   /**
    * Производит авторизацию с помощью User.login
    * После успешной авторизации, сбрасывает форму,
@@ -11,6 +11,17 @@ class LoginForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit( options ) {
+  	User.login(options, (err, data) => {
+      if (data.success) {
+        this.element.reset();
+        App.setState('user-logged');
 
+        let modal = new Modal(this.element.closest('.modal'));
+  			modal.close();
+  		} else {
+  			alert(data.error);
+  			return;
+  		}
+  	});
   }
 }
