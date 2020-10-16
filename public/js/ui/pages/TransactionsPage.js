d97a4857cc9ac47dc343cc4173189fa99b045b94
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-10-16 09:01:10 +0800

    Update TransactionsPage.js

--- public/js/ui/pages/TransactionsPage.js
+++ public/js/ui/pages/TransactionsPage.js
@@ -11,14 +11,18 @@ class TransactionsPage {
    * через registerEvents()
    * */
   constructor( element ) {
-
+    if(!element) {
+      throw new Error('Передан пустой элемент');
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
    * Вызывает метод render для отрисовки страницы
    * */
   update() {
-
+    this.render(this.lastOptions);
   }
 
   /**
@@ -28,7 +32,26 @@ class TransactionsPage {
    * TransactionsPage.removeAccount соответственно
    * */
   registerEvents() {
+    if (this.element.querySelector('.remove-account')) {
+      let elRemoveAccount = this.element.querySelector('.remove-account');
+      
+      elRemoveAccount.addEventListener('click', event => {
+        if (document.querySelector('.active')){
+          let elRemThisAccount = document.querySelector('.active').dataset.id;
+          event.preventDefault();
+          this.removeAccount(elRemThisAccount);
+        }
+      });
+    }
 
+    if (document.querySelector('.transaction__remove')) {
+      let elTransactionRemove = document.querySelector('.transaction__remove');
+      
+      elTransactionRemove.addEventListener('click', event => {
+        event.preventDefault();
+        this.removeTransaction(event.currentTarget.dataset.id);
+      });
+    }
   }
 
   /**
@@ -39,8 +62,21 @@ class TransactionsPage {
    * По успешному удалению необходимо вызвать метод App.update()
    * для обновления приложения
    * */
-  removeAccount() {
+  removeAccount(options) {
+    let elConfirm = confirm('Вы действительно хотите удалить счёт?');
 
+    if (elConfirm) {
+      Account.remove(options, {}, (err, data) => {
+
+        if(!err) {
+        
+          if(data.success) {
+            this.clear();
+            App.update();
+          }
+        }
+      });
+    }
   }
 
   /**
@@ -49,7 +85,20 @@ class TransactionsPage {
    * По удалению транзакции вызовите метод App.update()
    * */
   removeTransaction( id ) {
+    if(id){
+      let elConfirm = confirm('Вы действительно хотите удалить эту транзакцию?');
 
+      if (elConfirm) {
+        Transaction.remove(id, {}, (err, data) => {
+          if(!err) {
+
+            if(data.success) {
+              App.update();
+            }
+          }
+        });
+      }
+    }
   }
 
   /**
@@ -59,7 +108,25 @@ class TransactionsPage {
    * в TransactionsPage.renderTransactions()
    * */
   render( options ) {
+    if (options) {
+      this.lastOptions = options;
+      Account.get(options.account_id, {}, (err, data) => {
 
+        if(!err) {
+          this.renderTitle(data.data.name);
+        }
+      });
+
+      Transaction.list(options, (err, data) => {
+        if(!err) {
+
+          if(data.success) {
+            this.renderTransactions();
+            this.renderTransactions(data.data);
+          }
+        }
+      });
+    }
   }
 
   /**
@@ -68,14 +135,18 @@ class TransactionsPage {
    * Устанавливает заголовок: «Название счёта»
    * */
   clear() {
-
+    document.querySelector('.content').innerHTML = '';
+    this.renderTransactions();
+    this.renderTitle('Название счёта');
+    this.lastOptions = '';
   }
 
   /**
    * Устанавливает заголовок в элемент .content-title
    * */
   renderTitle( name ) {
-
+    let elContentTitle = document.querySelector('.content-title');
+    elContentTitle.textContent = name;
   }
 
   /**
@@ -83,7 +154,17 @@ class TransactionsPage {
    * в формат «10 марта 2019 г. в 03:20»
    * */
   formatDate( date ) {
+    let parseDate = new Date(date);
+    let arrMonth = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+    let tDate, tMonth, tYear, tHours, tMinutes;
+    
+    tDate = parseDate.getDate() < 10 ? (`0${parseDate.getDate()}`) : parseDate.getDate();
+    tMonth = arrMonth[parseDate.getMonth()];
+    tYear = parseDate.getFullYear();
+    tHours = parseDate.getHours() < 10 ? (`0${parseDate.getHours()}`) : parseDate.getHours();
+    tMinutes = parseDate.getMinutes() < 10 ? (`0${parseDate.getMinutes()}`) : parseDate.getMinutes();
 
+    return (`${tDate} ${tMonth} ${tYear} г. в ${tHours}:${tMinutes}`);
   }
 
   /**
@@ -91,14 +172,54 @@ class TransactionsPage {
    * item - объект с информацией о транзакции
    * */
   getTransactionHTML( item ) {
+    let elTransaction = document.createElement('DIV');
+    elTransaction.className = `transaction transaction_${item.type} row`;
+    elTransaction.innerHTML = `
+    <div class="col-md-7 transaction__details">
+      <div class="transaction__icon">
+          <span class="fa fa-money fa-2x"></span>
+      </div>
+      <div class="transaction__info">
+          <h4 class="transaction__title">${item.name}</h4>
+          <!-- дата -->
+          <div class="transaction__date">${this.formatDate(item.created_at)}</div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="transaction__summ">
+      <!--  сумма -->
+          ${item.sum} <span class="currency">₽</span>
+      </div>
+    </div>
+    <div class="col-md-2 transaction__controls">
+        <!-- в data-id нужно поместить id -->
+        <button class="btn btn-danger transaction__remove" data-id="${item.id}">
+            <i class="fa fa-trash"></i>  
+        </button>
+    </div>
+    `;
+    
+    elTransaction.querySelector('.transaction__remove').addEventListener('click', event => {
+      event.preventDefault();
+      this.removeTransaction(event.currentTarget.dataset.id);
+    });
 
+    return elTransaction;
   }
 
   /**
    * Отрисовывает список транзакций на странице
    * используя getTransactionHTML
    * */
-  renderTransactions( data ) {
+  renderTransactions(data) {
+    let elContent = document.querySelector('.content');
 
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        elContent.insertAdjacentElement('beforeEnd', this.getTransactionHTML(data[i]));
+      }
+    } else {
+      elContent.innerHTML = '';
+    }
   }
 }
